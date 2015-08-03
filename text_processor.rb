9e2ace909cf50a31090a98106432fc746312d873
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-08-03 20:56:30 +0200

    added pre and post texts

--- text_processor.rb
+++ text_processor.rb
@@ -1,6 +1,9 @@
 require './connection'
 
 text = ""
+pre_text = ""
+post_text = ""
+
 Template.all.each do |t|
   fits = true
   fits &= (t.Baustein_von <= 1)
@@ -11,7 +14,11 @@ Template.all.each do |t|
   fits &= (18 > t.Sensor_Max)
   fits &= (t.Baustein_Typ == "text")
   if fits
-    text << t.Baustein_Text
+    pre_text << t.Baustein_Vorspann.to_s
+    text << t.Baustein_Text.to_s
+    post_text << t.Baustein_Vorspann.to_s
   end
 end
+
+text = pre_text + text + post_text
 puts text.strip
